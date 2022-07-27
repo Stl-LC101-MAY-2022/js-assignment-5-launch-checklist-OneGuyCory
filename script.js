872b652e98c0fe02e0e51163db1705b4e9a5c4c0
Cory <corydeuschle92@gmail.com>
2022-07-27 17:23:12 -0500

    validateInput finished

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (event)=>{
+        
+        //List DOM
+        let pilot = document.querySelector("input[name=pilotName]").value;
+        let copilot = document.querySelector("input[name=copilotName]").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+        let list = document.getElementById('faultyItems');
+        let pilotStatus = document.getElementById('pilotStatus');
+        let copilotStatus = document.getElementById('copilotStatus');
+        let fuelStatus = document.getElementById('fuelStatus');
+        let launchStatus = document.getElementById('launchStatus');
+        let cargoStatus = document.getElementById('cargoStatus');
+
+        //use formsubmission to validate and update list
+        formSubmission(document,list,pilot,copilot,fuelLevel,cargoLevel);
+        event.preventDefault();
+    })
+    
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,48 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
+    if (testInput === ''|| testInput === null) {
+        return 'Empty';
+    } else if ((!isNaN(Number(testInput)))) {
+        return 'Is a Number';
+    } else {
+        return 'Not a Number';
+    };
    
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+
+    if (validateInput(pilot) === 'Empty' || validateInput(copilot) === 'Empty' || validateInput(fuelLevel) === 'Empty' || validateInput(cargoLevel) === 'Empty') {
+        alert('All Fields Are Required!');
+    };
+
+    if(validateInput(pilot) === 'Is a Number' || validateInput(copilot) === 'Is a number') {
+        alert('Invalid information. Please enter valid information for all fields!');
+    }
+    
+   if (validateInput(fuelLevel) === 'Not a Number' || validateInput(cargoLevel) === 'Not a Number') {
+        alert('Invalid information. Please enter valid information for all fields!');
+   }
+
+   pilotStatus.innerHTML = `Pilot ${pilot} ready.`
+   copilotStatus.innerHTML = `Copilot ${copilot} ready.`
+
+   if (fuelLevel < 10000) {
+    list.style.visibility ='visible';
+    fuelStatus.innerHTML = 'There is not enough fuel for the journey.';
+    launchStatus.innerHTML = 'Shuttle not ready for launch';
+    launchStatus.style.color ='red';
+   } else if(cargoLevel > 10000) {
+    list.style.visibility = 'visible';
+    cargoStatus.innerHTML = 'There is too much mass for the shuttle to take off.';
+    launchStatus.innerHTML = 'Shuttle not ready for launch';
+    launchStatus.style.color ='red';
+   } else if (fuelLevel > 10000 && cargoLevel < 10000) {
+    list.style.visibility = 'visible'
+    launchStatus.innerHTML = 'Shuttle is ready for launch.';
+    launchStatus.style.color = 'green';
+   }
 }
 
 async function myFetch() {
